488a0728d9d11a62f75b72e7d175ed83ac27c6ae
drchristopherhopkins <snazzyhoppy@aol.com>
2022-07-02 23:22:05 -0400

    4 files modified, 14 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ '../css/bootstrap.min.css',
+  '../css/style.css' ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
